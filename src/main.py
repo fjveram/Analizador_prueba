a4aa168d498457f10094b3f5f58d4d6c6df3ae75
Francisco Vera <franveramoran@gmail.com>
2026-09-17 11:16:22 +0200

    Integrando analizador ip en main

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
-print("Analizador IP/ Monitor de red")
-print("------------------------------")
+import ip_analyzer
+
+
+def main():
+    configuracion = ip_analyzer.obtener_configuracion_red()
+
+    if configuracion is None:
+        return
+
+    ip_analyzer.analizar_red(
+        configuracion["ip"],
+        configuracion["mascara"],
+        configuracion["gateway"]
+    )
+
+
+if __name__ == "__main__":
+    main()
